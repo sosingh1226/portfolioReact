0d009953ef6ba8f98ce380c94a897c89adb90f7c
soumyas <so.singh1226@gmail.com>
2021-03-12 23:04:20 -0500

    updated pages

---
+++ client/src/Components/Aboutpage.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Aboutpage = () => {
+  return (
+    <div>
+      <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="container">
+                    <div class="p-3 mb-2 bg-warning text-dark">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="page-header">
+                                    <h1>About Me</h1>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-12">
+                                <img class="image-responsive float-left wt-50" src="./Assets/Images/ss_headshot.jpeg" width="200" alt="placeholder headshot">
+                                <p>Dynamic full-stack web developer with expertise in more intuitive user experience on the web. Currently, enrolled in a full stack web development course from the University of California, Berkeley with newly developed skills in JavaScript, CSS, React.js, and responsive web design.</p>
+
+                                <p>I’m seeking entry level position in this field to achieve hands on professional experience and utilize my knowledge and skills with developers of all levels. I love to work in a fast, high-growth environment that values its employees and customers. I’m a self-starter who enjoys collaborating with other engineers and solving complicated problems.</p>
+                                    
+                                <p>Prior to my current education, I accomplished a Certificate in UX Design from General Assembly, Boston that further motivated me to deep dive into Web Development. I am very proud to present my academic capstone project with real user/ human behavior interaction to study and enhance use cases in the field of first time home owners trying to find different vendors & quotes while optimizing their valuable time, efforts & money. The design process includes a website and a clickable prototype of an Android App, using different ways of research, creating personas, and continuously iterating the final product based on User Testing.</p>
+                                    
+                                <p>Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience. I applied aspects of UX and agile development in a recent project. I’m currently working in a team of four to develop a single-page MERN app that helps small scale neighborhood businesses to collaborate with the residents to help grow their sales and strengthen a sense of community. I’m excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.</p>
+                                    
+                                <p>10+ years of work experience in the field of Architecture & Design highlights developing concepts and being able to fully express my design-thinking process with my team & clients. My work responsibilities developed a sense of urgency,  importance of meeting deadlines, continuous on-going communication with the team, sense of decision making, client communication, and leadership quality while being a problem solver at all levels of a project.</p>
+                                    
+                                <p>Technical Skills- Invision, Sketch, SketchUp, HTML, JavaScript, JSON, CSS, jQuery, Bootstrap, Node.js, MySQL, MongoDB, React.js, C++, Front end Web Design, Web & App prototyping, MS Office, Adobe Photoshop & Indesign.</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Aboutpage
---
+++ client/src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div class="container">
+            <ul>so.singh1226@gmail.com</ul>
+            <ul>+1.508.309.2842</ul>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
